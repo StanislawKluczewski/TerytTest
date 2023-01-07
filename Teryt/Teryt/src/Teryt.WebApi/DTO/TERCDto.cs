c36b02d2601b797fde675a15dc23adf35ac5283a
StaKluczewski <stakluczewski@gmail.com>
2023-01-07 12:59:53 +0100

    Poprawa Dto
    Poprawa Dto

--- Teryt/Teryt/src/Teryt.WebApi/DTO/TERCDto.cs
+++ Teryt/Teryt/src/Teryt.WebApi/DTO/TERCDto.cs
@@ -2,5 +2,8 @@
 {
     public class TERCDto
     {
+        public string JednostkaPodzialu { get; set; }
+        public string Powiat { get; set; }
+        public string Wojewodztwo { get; set; }
     }
 }
